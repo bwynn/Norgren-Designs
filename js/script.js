852fe93f40becdb1fe88cc75b9bd141f9dc19965
Brian Wynn <brian.wynn83@gmail.com>
2015-01-18 23:13:35 -0800

    brought out nav-line border

--- js/script.js
+++ js/script.js
@@ -1,22 +1,26 @@
 // JavaScript Document
 
-// targets button with ID attribute of "send"
-var btn = document.getElementById("send");
+//User confirmation
+function confirm() {
+	// targets button with ID attribute of "send"
+	var btn = document.getElementById("send");
+	// user notification of successful query submission
+	function showAlert() {
+		alert("Thank you for your inquiry. We'll get back to you shortly.");
+		}
+	// attaches showAlert function when button is clicked
+	btn.addEventListener("click", showAlert);
+}
 
-// user notification of successful query submission
-var showAlert = function() {
-	alert("Thank you for your inquiry. We'll get back to you shortly.");
-	}
-
-// attaches showAlert function when button is clicked
-btn.addEventListener("click", showAlert);
-
-$(document).ready(function() {
-	$("nav > li > a").on("click", "a", function(){
-	
-		});
-	});
-
-
-	
-	
+//Validation
+//function contactValidate() {
+	//get input fields
+	//get length of characters
+	//if char > 3
+			//valid
+	// char < 3
+			//apply class for alert box
+			//show message
+//}
+confirm();
+//contactValidation();
