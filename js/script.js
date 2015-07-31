91c93ade2199672ce49cc55994f5d4bcedfae93e
Brian Wynn <brian.wynn83@gmail.com>
2015-07-31 00:53:56 -0700

    Added styles to content areas for all pages.

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ var nd = {
 	shell: {
 		// load dom content
 	 	configMap: {
+			// create cont object which populates the #nd element
 			cont: String()
 				+ "<header id='globalHeader'>"
 					+ "<div id='logo-wrap' class='center'>"
@@ -34,6 +35,7 @@ var nd = {
 				final_position: 10 + "%"
 		}
 	},
+	// this object contains the content for the home page
 	homePageModule : {
 		// content map object holds all of the content being generated on the page.
 		contentMap: {
@@ -42,9 +44,11 @@ var nd = {
 			home_p2: "Our design philosophy – An uncommon perspective. Engineering is often perceived as a dry, technical, numbers game, transforming ideas into products through mathematical tools and methods. Norgren Designs understands that wiz-bang 3D modeling, FEA, and other technical engineering practices are tools that can easily be wasted without a solid foundation on which to apply them. We value the highly creative stage in the design process that is often overlooked and undervalued. We excel at this creative stage by leveraging experience and intuition to quickly arrive at an optimal starting point. Any design can be optimized in a technical sense, but not every optimized design is an optimal solution. We focus on the creative stage up front to set your project up for success later. Our motto: Creativity, Quantified."
 		}
 	},
+	// contains the content for the about page
 	aboutModule: {
 		// content map object holds all of the content being generated on the page.
 		contentMap: {
+			title: "About",
 			about_p: "Erik comes from a family with multiple generations of creative engineers - engineering is in his blood. He has a Masters in Mechanical Engineering from the University of Colorado, Boulder, and 15+ years' experience designing mechatronic systems from the ground up. Robotics and mechanism design are his specialties. He has a proven ability to create successful products from primitive concepts. He enjoys rock climbing and hiking in his spare time.",
 			collaborators_p: "I am continuously making progress on a handful of in-house development projects alongside client-based projects. Think think-tank meets technical competency. I am looking for creative minds to augment my mechanical engineering technical expertise. If you have an innate ability to do what you do, are self-motivated, and want to test your abilities in a new setting, I am interested in hearing from you."
 		}
@@ -58,10 +62,10 @@ var contentID = [
 ];
 
 // constructor function for page content
-var content = function(arg1, arg2, arg3) {
-	arg1.text(nd.homePageModule.contentMap.title);
-	arg2.text(nd.homePageModule.contentMap.home_p1);
-	arg3.text(nd.homePageModule.contentMap.home_p2);
+var content = function(arg1, arg2, arg3, arg4, arg5, arg6) {
+	arg1.text(arg4);
+	arg2.text(arg5);
+	arg3.text(arg6);
 };
 
 var homeContent = function() {
@@ -69,14 +73,14 @@ var homeContent = function() {
 	var title = $("#homePageContent h1"),
 			p1 = $("#homePageContent p:first"),
 			p2 = $("#homePageContent p:last");
-	return content(title, p1, p2);
+	return content(title, p1, p2, nd.homePageModule.contentMap.title, nd.homePageModule.contentMap.home_p1, nd.homePageModule.contentMap.home_p2 );
 };
 
 var aboutContent = function() {
 	var title = $("aboutPageContent h1"),
 			about = $("#aboutPageContent p:first"),
 			collaborators = $("#aboutPageContent p:last");
-	return content( title, about, collaborators );
+	return content( title, about, collaborators, nd.aboutModule.contentMap.title, nd.aboutModule.contentMap.about_p, nd.aboutModule.contentMap.collaborators_p );
 };
 
 var contentLogic = function() {
@@ -105,13 +109,16 @@ var activeSwitch = function() {
 		$(this).addClass("active");
 
 		if (e.target == $navLink[0]) {
-			return content.attr("id", contentID[0]);
+			content.attr("id", contentID[0]);
+			return contentLogic();
 		}
 		else if (e.target == $navLink[1]) {
-			return content.attr("id", contentID[1]);
+			content.attr("id", contentID[1]);
+			return contentLogic();
 		}
 		else if (e.target == $navLink[2]) {
-			return content.attr("id", contentID[2]);
+			content.attr("id", contentID[2]);
+			return contentLogic();
 		}
 		else { return false; }
 	});
