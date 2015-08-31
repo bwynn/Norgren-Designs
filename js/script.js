e004c9eae3eb33440d4336bb29896c3102bfdb19
Brian Wynn <brian.wynn83@gmail.com>
2015-08-30 18:18:08 -0700

    Added some notes to the toggle nav method.

--- js/script.js
+++ js/script.js
@@ -114,6 +114,15 @@ var buildPageContent = function( obj, str ) {
 
 // ------------------------ DOM METHODS ----------------------------------------
 // Begin DOM method /toggleNav/
+// Purpose: To set the navigation display on devices smaller than a viewport width of 768px.
+// Arguments: Content section and background modal section. -- to be defined in a later build, after dom
+// is established in an html build. For the time being, this gets a boolean value to
+// determine what state the mobile navigation menu should be in, as well as the mobile background
+// container. If the mobile nav button state has been assigned the display property of block,
+// this will set a display property state change on the mobile nav element. It also switches the order
+// in which each section is shown/hidden, to give a transition effect where the background is
+// layed down first, and then the nav is revealed.
+//
 var toggleNav = function() {
 	var cont = $("#mobileNav > nav"),
 			background = $(".nav-background");
