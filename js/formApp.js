9d92169cc6f347b3076f8b4c62acab9f63d92d81
Brian Wynn <brian.wynn83@gmail.com>
2015-09-03 22:08:13 -0700

    Altered the controller alert values.

--- js/formApp.js
+++ js/formApp.js
@@ -1,9 +1,11 @@
 angular.module("formApp", [])
 .controller("formCtrl", function( $scope ) {
   $scope.addUser = function( userDetails ) {
-    alert("Thanks " + userDetails.name + ", we'll be in touch shortly.");
-    localStorage.name = userDetails.name;
-    localStorage.email = userDetails.email;
-    localStorage.msg = userDetails.msg;
-  };
+    if (ndForm.$valid) {
+      alert("Thanks " + userDetails.name + ", we'll be in touch shortly.");
+    }
+    else {
+      $scope.showValidation = true;
+    }
+  }
 });
