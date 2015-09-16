9633e5c2a62e671e1da1b030e72bdfdff3e65543
Brian Wynn <brian.wynn83@gmail.com>
2015-09-16 00:40:48 -0700

    created ndApp.js document and placed controllers for both sections into the document. Added styles as well.

--- js/formApp.js
+++
@@ -1,6 +0,0 @@
-angular.module("formApp", [])
-.controller("formCtrl", function( $scope ) {
-  $scope.addUser = function( userDetails ) {
-    alert("Thanks " + userDetails.name + ", we'll be in touch shortly.");
-  }
-});
