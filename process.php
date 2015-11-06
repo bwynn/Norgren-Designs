0c62ca6ccc0ccd4f2ad1e18acaa18b6f3c9ec8da
Brian Wynn <brian.wynn83@gmail.com>
2015-11-05 19:23:08 -0800

    Added IE fixes for media query declarations in 1x resolution screens.

--- process.php
+++ process.php
@@ -1,8 +1,8 @@
-<?php 
+<?php
 	$message = '';
 	foreach($_POST as $key => $value) {
-		$message .= "$key => $value\n"; 
+		$message .= "$key => $value\n";
 		}
-	mail('brian.wynn83@gmail.com','Contact Form Submission',$message);
+	mail('erik@norgrendesigns.com','Contact Form Submission',$message);
 	header('Location: index.html');
 ?>
