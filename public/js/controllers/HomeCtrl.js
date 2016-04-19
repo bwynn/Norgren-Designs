98ca8b142262fd344df1879ca79cf888142ec3b1
Brian Wynn <brian.wynn83@gmail.com>
2016-04-18 23:19:48 -0700

    Working out the background array and trying to reverse engineer it, while late at night. picking this up tomorrow.

--- public/js/controllers/HomeCtrl.js
+++ public/js/controllers/HomeCtrl.js
@@ -1,32 +1,32 @@
 // public/js/controllers/HomeCtrl.js
 angular.module("HomeCtrl", [])
   .controller("HomeController", ["$scope", function($scope) {
-    $scope.backgrounds = [];
+    $scope.backgrounds = ["bolt", "cable", "card", "circuitboard", "coffee", "cylinder", "hardware", "input", "prototypes"];
 
     // begin model function /getClass/
     // Purpose: this function is run for each array item in the background content
     // section. It pulls a random item out of the background.class array and then
     // removes that item from the array.
     $scope.getClass = function() {
-      var backgrounds = [
-        "bolt",
-        "cable",
-        "card",
-        "circuitboard",
-        "coffee",
-        "cylinder",
-        "hardware",
-        "input",
-        "prototypes"
-      ];
-    	// get random string from array, will hold class value
-     	var random = Math.floor(Math.random() * backgrounds.length),
-    	 		cur_class = backgrounds.splice( random, 1 );
 
-    	while (backgrounds.length >= 0) {
-    		$scope.backgrounds.push(cur_class);
-    	}
+    	for (var i = 0; i < backgrounds.length; i++) {
+        // get random string from array, will hold class value
+       	var random = Math.floor(Math.random() * backgrounds.length);
+        $scope.backgrounds.push(random);
+        backgrounds.splice( random, 1 );
+      }
+      console.log($scope.backgrounds);
     };
     // end model function /getClass/
+
+
+    // THIS IS ALL SCREWED UP RIGHT NOW. FIX THIS TOMORROW...
+    $scope.randomClasses = function() {
+    var backgrounds = [];
+		var newLength = backgrounds.length;
+		for (var i = 0; i < newLength; i++) {
+			elem[i].classList.add( buildArray( background.class ) );
+		}
+	};
 
   }]);
