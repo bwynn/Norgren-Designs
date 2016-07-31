44937eabaf298413b186e083e2e563684be41e5d
Brian Wynn <brian.wynn83@gmail.com>
2016-07-31 01:57:06 -0700

    Following my previous tracks.

--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-  database: "mongodb://localhost/norgrendesigns"
+  database: "mongodb://localhost/norgrendesigns",
+  uri: 'mongodb://heroku_lxdl6mj6:gmv2uui3ntcm3qm09euqofnjl6@ds015942.mlab.com:15942/heroku_lxdl6mj6'
 };
